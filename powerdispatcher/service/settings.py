f4abd7116f92f5a2db2a2248d1ed438441fb4025
Diego Negrette <diegonegrette42@gmail.com>
2024-03-29 12:36:18 -0400

    Added allowed hosts

--- powerdispatcher/service/settings.py
+++ powerdispatcher/service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env.str(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env.str("ALLOWED_HOSTS", default="127.0.0.1")]
 
 
 # Application definition
